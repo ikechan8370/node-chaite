e90264989acfbca6522ce19932e221b99d424b85
时瑾 <74231782+sj817@users.noreply.github.com>
2025-11-07 12:03:00 +0800

    feat: tsdown 打包为esm

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -14,14 +14,10 @@ import cors from 'cors'
 import { authenticateToken } from './middlewares'
 import { getLogger } from '../utils'
 import path from 'path'
-import { fileURLToPath } from 'url'
 import type { Server } from 'node:http'
+import { __dirname } from '../__dirname'
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-
-
-export function createApp(configure?: (app: Application) => void): Application {
+export function createApp (configure?: (app: Application) => void): Application {
   const app = express()
   app.use(express.json())
   app.use(cors())
@@ -37,14 +33,14 @@ export function createApp(configure?: (app: Application) => void): Application {
   app.use('/api/toolGroups', authenticateToken, ToolGroupsRouter)
   app.use('/v1', authenticateToken, OpenAIRouter)
   configure?.(app)
-  app.use(express.static(path.join(__dirname, './frontend/build')))
+  app.use(express.static(path.join(__dirname, '../frontend/build')))
   app.get('*', (_req, res) => {
-    res.sendFile(path.join(__dirname, './frontend/build/index.html'))
+    res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
   })
   return app
 }
 
-export function runServer(host: string, port: number, configure?: (app: Application) => void): { app: Application, server: Server } {
+export function runServer (host: string, port: number, configure?: (app: Application) => void): { app: Application, server: Server } {
   const app = createApp(configure)
   const server = app.listen(port, host, () => {
     getLogger().info(`Chaite server is running at http://${host}:${port}`)
